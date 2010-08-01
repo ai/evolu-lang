1e2ccbff425f7e79a07c5d0d24d7dc6ec4db135a
Andrey A.I. Sitnik <andrey@sitnik.ru>
2010-08-01 21:25:04 +0400

    Add method to add condition to language

--- spec/unit/language.js
+++ spec/unit/language.js
@@ -39,15 +39,43 @@ JSpec.describe('darwin.lang', function() {
     
     it('should add commands', function() {
         var lang = evolu.lang('LNG')
+        var run = function() { }
         
-        var returned = lang.command('one', { a: 1 })
+        var returned = lang.command('one', run)
         expect(returned).to(be, lang)
-        expect(lang._commands).to(eql, [{ name: 'one', a: 1 }])
+        expect(lang._commands).to(eql, [{ name: 'one', run: run }])
         
         lang.command('three', { c: 3 })
         lang.command('two',   { b: 2, position: 1 })
-        expect(lang._commands).to(eql, [{ name: 'one',   a: 1 },
+        expect(lang._commands).to(eql, [{ name: 'one',   run: run },
                                         { name: 'two',   b: 2 },
                                         { name: 'three', c: 3 }])
+    })
+    
+    it('should add condition', function() {
+        var lang = evolu.lang('LNG')
+        var run = function() { }
+        var init = lang._initCondition
+        
+        var returned = lang.condition('one', run)
+        lang.condition('three', { c: 3 })
+        lang.condition('two',   { b: 2, position: 1 })
+        
+        expect(returned).to(be, lang)
+        expect(lang._commands).to(eql, [
+            { name: 'if_one',   run: run, init: init, condition: true },
+            { name: 'if_two',   b: 2,     init: init, condition: true },
+            { name: 'if_three', c: 3,     init: init, condition: true }
+        ])
+    })
+    
+    it('should set own init for condition', function() {
+        var lang = evolu.lang('LNG')
+        lang._initCondition = function(i) { this.push(i + '_global') }
+        lang.condition('one', { init: function(i) { this.push(i) } })
+        
+        var out = []
+        lang._commands[0].init.call(out, 'a')
+        expect(out).to(eql, ['a_global', 'a'])
     })
 })
