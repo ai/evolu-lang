2e65331923b7cef5d5cac1e5b70beaf0847bb2eb
Andrey A.I. Sitnik <andrey@sitnik.ru>
2010-08-02 18:41:16 +0400

    Add method to compile bytes to program rules

--- spec/unit/language.js
+++ spec/unit/language.js
@@ -32,10 +32,11 @@ JSpec.describe('darwin.Language', function() {
         var lang = evolu.lang('LNG', function() {
             this.command('one',   func).
                  command('three', { c: 3 }).
-                 command('two',   { b: 2, position: 1 })
+                 command('two',   { b: 2, position: 2 })
         })
         
-        expect(lang._commands).to(eql, [{ name: 'one',   run: func },
+        expect(lang._commands).to(eql, ['separator',
+                                        { name: 'one',   run: func },
                                         { name: 'two',   b: 2 },
                                         { name: 'three', c: 3 }])
     })
@@ -44,10 +45,11 @@ JSpec.describe('darwin.Language', function() {
         var func = function() { }
         var lang = evolu.lang('LNG', function() {
             this.condition('b', { b: 2 }).
-                 condition('a', { a: 1, position: 0 })
+                 condition('a', { a: 1, position: 1 })
         })
         
         expect(lang._commands).to(eql, [
+            'separator',
             { name: 'if_a', a: 1, init: lang._initCondition, condition: true },
             { name: 'if_b', b: 2, init: lang._initCondition, condition: true }
         ])
@@ -60,7 +62,7 @@ JSpec.describe('darwin.Language', function() {
         })
         
         var out = []
-        lang._commands[0].init.call(out, 'a')
+        lang._commands[1].init.call(out, 'a')
         expect(out).to(eql, ['a_global', 'a'])
     })
     
@@ -74,10 +76,37 @@ JSpec.describe('darwin.Language', function() {
         }).to(throw_error, 'Unknown Evolu language `LNG`')
         
         var lang = evolu.lang('LNG', function() { })
-        expect(lang).to(receive, 'compile', 'once').with_args([97, 98, 99])
+        stub(lang, 'compile')
+        expect(lang).to(receive, 'compile').with_args([97, 98, 99])
         var result = evolu.compile('EVOLU:LNG:abc')
+    })
+    
+    it('should compile bytes to rules', function() {
+        var lang = evolu.lang('LNG', function() {
+            this.command('a', { params: ['one', 'two'] }).
+                 command('b', { init: function() {} })
+        })
+        var a = lang._commands[1], b = lang._commands[2]
         
-        expect(result).to(be_an_instance_of, evolu.Code)
-        expect(result).to(have_property, 'language', lang)
+        var code = lang.compile([128, 5, 0, 1, 128, 131, 2, 130, 128])
+        
+        expect(code).to(be_an_instance_of, evolu.Code)
+        expect(code).to(have_property, 'language', lang)
+        
+        expect(code._rules).to(eql, [[ [b] ], [ [a, 'two'], [b, 256] ]])
+    })
+    
+    it('should call initializers on compiling', function() {
+        var lang = evolu.lang('LNG', function() {
+            this.command('a', { init: function() { }, params: ['one', 'two'] }).
+                 command('b', { init: function() { } })
+        })
+        var a = lang._commands[1], b = lang._commands[2]
+        
+        var rule = [[a, 'one'], [b]]
+        expect(a).to(receive, 'init').with_args(rule, 'one')
+        expect(b).to(receive, 'init').with_args(rule)
+        
+        lang.compile([1, 128, 2])
     })
 })
